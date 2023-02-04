a6ab07e4198d99e714a84be4c289ba4dce328889
oleg086rus <o.pototskiy@yandex.ru>
2023-02-04 16:24:06 +0500

    some changes in client, add rrd, need to websoket

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import './styles/app.scss'
 import Toolbar from './components/Toolbar'
 import SettingBar from './components/SettingBar'
 import Canvas from './components/Canvas'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import MainPage from './page/MainPage'
 
 function App() {
   return (
-    <div className="App">
-      <Toolbar/>
-      <SettingBar/>
-      <Canvas/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/:id' element={<MainPage/>}/>
+          <Route path='*' element={<Navigate to={`f${(+new Date).toString(16)}`} replace/>}/>
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
